489bfb8ab6ba0a607e542ce1190587f481ab1c20
Yuriy Shefer <sheff@sheffmachine.com>
2016-06-12 20:24:49 -0700

    add the options functionality to set channel and max number of results

--- js/app.js
+++ js/app.js
@@ -2,12 +2,43 @@ $(document).ready(function () {
   document.addEventListener('deviceready', onDeviceReady, false);
 
   function onDeviceReady () {
-    var channel1 = 'TechGuyLabs';
-    getPlaylist(channel1);
+    //check local storage for channel
+    if (!localStorage.channel) {
+      //ask user for a channel
+      $('#popupdialogue').popup('open');
+    } else {
+      var channel = localStorage.getItem('channel');
+    }
+    $('#channelbuttonok').click(function () {
+      var channel = $('#channelname').val();
+      setChannel(channel);
+      getPlaylist(channel);
+    });
+    $('#saveoptions').click(saveOptions);
+    $('#clearchannel').click(clearChannel);
+    getPlaylist(localStorage.getItem('channel'));
     $(document).on('click', '#vidlist li', function () {
       var id =$(this).attr('videoid');
       showVideo(id);
     });
+  }
+
+  $(document).on('pageinit', '#options', function (e) {
+    console.log('Options Page Loaded');
+    var channel = localStorage.getItem('channel');
+    var maxResults = localStorage.getItem('maxresults');
+    $('#channelnameoptions').val(channel);
+    $('#maxresultsoptions').val(maxResults);
+  });
+
+  function setChannel (channel) {
+    localStorage.setItem('channel', channel);
+    console.log('Channel Set ' + channel);
+  }
+
+  function setMaxResults (results) {
+    localStorage.setItem('maxresults', results);
+    console.log('Max Results set to ' + results);
   }
 
   function getPlaylist (channel) {
@@ -21,7 +52,8 @@ $(document).ready(function () {
       $.each(data.items, function (i, item) {
         console.log(item);
         var playlistId = item.contentDetails.relatedPlaylists.uploads;
-        getVideos(playlistId, 10);
+        var maxResults = localStorage.getItem('maxresults') || 10;
+        getVideos(playlistId, maxResults);
       });
     });
   }
@@ -48,5 +80,23 @@ $(document).ready(function () {
     $('#logo').hide();
     var output = '<iframe width="100%" height="250" src="https://www.youtube.com/embed/' + id + '" frameborder="0" allowfullscreen></iframe>';
     $('#showVideo').html(output);
+  }
+
+  function saveOptions () {
+    var channel = $('#channelnameoptions').val();
+    setChannel(channel);
+    var maxResults = $('#maxresultsoptions').val();
+    setMaxResults(maxResults);
+    $('body').pagecontainer('change', '#main');
+    getPlaylist(channel);
+  }
+
+  function clearChannel () {
+    localStorage.removeItem('channel');
+    $('body').pagecontainer('change', '#main');
+    //clear list
+    $('#vidlist').html('');
+    //show popup
+    $('#popupdialogue').popup('open');
   }
 });
